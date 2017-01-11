f2cce2ab2a6ea3d0c2e6e64a9ec74a6a281b82f8
SaitamaCreator <gab.abreu23@gmail.com>
2017-01-11 18:29:50 -0300

    Add files via upload

---
+++ Aviao.h
@@ -0,0 +1,25 @@
+#ifndef AVIAO_H
+#define AVIAO_H
+
+class Aviao
+{
+public:
+    Aviao();
+    ~Aviao();
+    
+    void Aviao(double alt){
+        setAltura(alt);
+        };
+    void Aviao( ){   
+        setAltura(0)
+    }
+    serAltura(int i){
+        altura << i;
+        }
+    
+private:
+    double Altura;
+
+};
+
+#endif // AVIAO_H
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cout;
+
+#include "Aviao.h"
+
+int main(int argc, char **argv)
+{
+    
+	cout <<"Fala Migo!";
+    return 0;
+}
