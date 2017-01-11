75e73e657ffd7fe725821a28bc2067b0c2f1fe81
FACOMP <FACOMP@LABM-PC15>
2017-01-11 16:59:07 -0300

    Aviao

--- main.cpp
+++ main.cpp
@@ -1,44 +1,12 @@
 #include<iostream>
+#include<"Aviao.h">
 using std::cout;
 using std::cin;
 using std::endl;
 
-class  Aviao{
-        private:
-            float altitude=0, variacao;
-        
-        public:
-        	bool estado=true;
-            void imprimirAltitude(){
-                cout << "altitude atual: " << altitude << endl;
-            };   
-            
-            void alterarAltitude(){
-                cout << "Entre com a variacao de altitude: ";
-                cin >> variacao;
-            	altitude=altitude+variacao;
-            	if(altitude<0||altitude>1000){
-                    estado=false;
-            	};
-        	};
-};
-
 
 int main(){
     
-    Aviao myAviao;
-    
-    cout << "VAMOS VOAR! Controle a altidude de aviao, de 0 a 1000 pes, ou ele será destruido"<<endl;
-    
-    myAviao.imprimirAltitude();
-    
-    do{
-        myAviao.alterarAltitude();
-        
-        myAviao.imprimirAltitude();    
-		    
-    }while(myAviao.estado==true);
-    
-    cout << endl << endl <<"                            YOU LOSE!";
+	cout <<"hello World";
     
 };
